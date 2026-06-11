9995e70b21404a0d73e86f4c72da19c0a18d6d4b
Nicolás Fenoglio <57574930+fnAlfa@users.noreply.github.com>
2026-06-11 20:00:48 -0300

    fix: express importado correctamente

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
+import express from "express";
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, World!');
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, World!");
 });
 
 app.listen(port, () => {
