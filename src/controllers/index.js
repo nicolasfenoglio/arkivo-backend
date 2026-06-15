3fde7280b430e4f0134d61a6be76151e7ba0433f
Nicolás Fenoglio <57574930+fnAlfa@users.noreply.github.com>
2026-06-15 00:38:54 -0300

    feat: agregué la estructura general del proyecto

---
+++ src/controllers/index.js
@@ -0,0 +1,6 @@
+/**
+ * En este archivo se importan y exportan todos los controladores de la aplicación.
+ * Esto permite una gestión centralizada de los controladores y facilita su importación en otras partes de la aplicación.
+ * Cada controlador se encarga de manejar la lógica de negocio relacionada con una entidad específica (por ejemplo, usuarios, productos, etc.).
+ * Al exportar todos los controladores desde este archivo, se mejora la organización del código y se facilita su mantenimiento.
+ */
---
+++ src/middlewares/index.js
@@ -0,0 +1,6 @@
+/**
+ * Los middlewares son funciones que se ejecutan durante el ciclo de vida de una solicitud HTTP en una aplicación Express.
+ * Estas funciones pueden modificar la solicitud (req) y la respuesta (res), o incluso finalizar el ciclo de solicitud-respuesta.
+ * En este archivo se importan y exportan todos los middlewares de la aplicación, lo que permite una gestión centralizada de ellos.
+ * Al tener un punto único de importación para los middlewares, se mejora la organización del código y se facilita su mantenimiento.
+ */
