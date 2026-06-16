0b2dff6db2d7abf25100e047f70a7b5dd9305fbb
Nicolás Fenoglio <57574930+fnAlfa@users.noreply.github.com>
2026-06-16 01:13:40 -0300

    refactor: movi la app a app.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import "dotenv/config";
 
-import express from "express";
-import Bucket from "./services/s3_bucket.service.js";
-
 [
   "S3_ENDPOINT",
   "S3_ACCESS_KEY",
@@ -17,13 +14,9 @@ import Bucket from "./services/s3_bucket.service.js";
   }
 });
 
-const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+import app from "./app.js";
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello, World!");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
--- src/services/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Los servicios son funciones que contienen la lógica de negocio de la aplicación y se encargan de realizar operaciones específicas, como interactuar con la base de datos, procesar datos, etc.
- * En este archivo se importan y exportan todos los servicios de la aplicación, lo que permite una gestión centralizada de ellos.
- * Al tener un punto único de importación para los servicios, se mejora la organización del código y se facilita su mantenimiento.
- */
