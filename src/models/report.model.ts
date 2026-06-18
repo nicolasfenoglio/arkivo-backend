b96335069ed8f1931c42d335d347f19e77c8dd22
Juan David Eggel <juandaeggel@gmail.com>
2026-06-18 19:00:40 -0300

    agregue modelos

---
+++ src/models/report.model.ts
@@ -0,0 +1,41 @@
+import {
+  Sequelize,
+  DataTypes,
+  Model,
+  type InferAttributes,
+  type InferCreationAttributes,
+  type CreationOptional,
+} from "@sequelize/core";
+import {
+  Attribute,
+  PrimaryKey,
+  AutoIncrement,
+  NotNull,
+  Unique,
+} from "@sequelize/core/decorators-legacy";
+
+export class Report extends Model<
+  InferAttributes<Report>,
+  InferCreationAttributes<Report>
+> {
+  @Attribute(DataTypes.INTEGER)
+  @PrimaryKey
+  @AutoIncrement
+  declare id: CreationOptional<number>;
+
+  @Attribute(DataTypes.INTEGER)
+  @NotNull
+  declare noteId: number;
+
+  @Attribute(DataTypes.INTEGER)
+  @NotNull
+  declare reporterId: number;
+
+  @Attribute(DataTypes.STRING)
+  @NotNull
+  declare reason: string;
+
+  @Attribute(DataTypes.STRING)
+  @NotNull
+  declare state: string;
+}
