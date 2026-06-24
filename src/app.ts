1aeaf0c17b24042092aee662877a9300bddcb77c
Nicolás Fenoglio <57574930+fnAlfa@users.noreply.github.com>
2026-06-23 21:25:20 -0300

    fix: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import profileRouter from "./routes/profiles.route.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello, World!");
